9a80c7c6b383aac720e5ed6167953d105b5c90ec
Carl Magnus Traberg <carlmagnus.traberg@gmail.com>
2026-05-29 16:23:12 +0200

    Header.tsx WIP

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,1 +1,19 @@
-import { useEffect, useState } from "react";
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header (){
+    return (
+    <header>
+        <nav className="nav">
+            <Link className="logo-link" href="/">
+            
+            </Link>
+
+        </nav>
+
+    </header>
+    )
+}
+
