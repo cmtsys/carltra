9947f4cdbb132d6f0cd77a55a98d75e4a0a5cabb
cmt-111 <56823667+cmt-111@users.noreply.github.com>
2026-08-01 21:37:10 +0200

    structural css fixes, case study on its way, might turn the whole thing into component driven layout.

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -6,12 +6,14 @@ export default function AboutMe() {
         <section className="aboutme">
             <img className="cm2" src="img/cm.png" alt="I look something like this" />
             <section className="about-text">
-                <h1 className="heading-serif">Hi there!</h1>
-                <p className="body-l">My name is Carl, I do UX, interaction design, graphics, a touch of coding, and a lot more. <br /> <br />
-                    {/* <p className="heading-md">My name is Carl, I am a UX designer who codes. Currently developing AI experiences at Huddly<br /> <br /> */}
-                    I built this thing myself*, have a look <a className="weblink" href="https://github.com/cmtsys/carltra" target="_blank">here</a> if you want proof. </p>
+                <div className="about-intro">
+                    <h1 className="heading-serif">Hi there!</h1>
+                    <p className="body-l">My name is Carl, I do UX, interaction design, graphics, a touch of coding, and a lot more. <br /> <br />
+                        I built this thing myself*, have a look <a className="weblink" href="https://github.com/cmtsys/carltra" target="_blank">here</a> if you want proof. </p>
+                </div>
                 <p className="small-text">*ok, I might have asked claude for help a few times, but no vibecode, I promise!</p>
             </section>
         </section>
     )
 }
+/* <p className="heading-md">My name is Carl, I am a UX designer who codes. Currently developing AI experiences at Huddly<br /> <br /> */
--- app/projects/daydream/page.tsx
+++ app/projects/daydream/page.tsx
@@ -5,41 +5,45 @@ export default function daydream() {
     {/* <h1 className="heading-lg">Daydreams & Dommscroll</h1> */}
     <p className="label">Master thesis at aho</p>
     <h1 className="heading-serif">Daydreams & Dommscroll</h1>
+
     <Image
       src="/img/daydream2.png"
       className='title-image'
       alt="Main image"
       width={8640}
       height={4860}
-    // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      sizes="(max-width: 900px) 100vw, 1000px"
     />
 
-      <div className="info">
+    <div className="info">
 
-        <div className="info-box">
-          <dt className="label-s">Year</dt>
-          <dd className="body-s">2026</dd>
-        </div>
-
-        <div className="info-box">
-          <p className="info-title info-text">Role</p>
-          <p className="info-text">UX Designer</p>
-        </div>
-
-        <div className="info-box">
-          <p className="info-title info-text">Skills</p>
-          <p className="info-text">Figma, Indesign, Research</p>
-        </div>
-
-        <div className="info-box">
-          <p className="info-title info-text">Client</p>
-          <p className="info-text">Oslo School of Architecture and Design</p>
-        </div>
-
+      <div className="info-box">
+        <dt className="label-s">Year</dt>
+        <dd className="body-s">2026</dd>
       </div>
+
+      <div className="info-box">
+        <dt className="label-s">Role</dt>
+        <dd className="body-s">UX Designer</dd>
+      </div>
+
+      <div className="info-box">
+        <dt className="label-s">Skills</dt>
+        <dd className="body-s">Figma, Indesign, Research</dd>
+      </div>
+
+      <div className="info-box">
+        <dt className="label-s">Client</dt>
+        <dd className="body-s">Oslo School of Architecture and Design</dd>
+      </div>
+
+    </div>
 
     <div className="prose">
 
+      <p className='body-xl'>Digital introspection is an interaction design project looking at new ways of contextualizing screen usage, disconnecting, and preserve introspective mind-states.</p>
+
+      <p className='body-l'>The project consists of three scenarios, each exploring a theme relating to digital wellbeing, displaying a breadth of possibilities and new opportunities for how we define our relationship to our devices.</p>
 
     </div>
 
