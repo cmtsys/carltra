458db09183d3e9bd6b0667b2682c034451935824
cmt-111 <56823667+cmt-111@users.noreply.github.com>
2026-09-09 20:30:53 +0200

    reMarkable case study under way, some content to fix, but we are getting there. Also, remember to clean up the css. Also, made new section structure. project-blocks -> case-section -> prose/figure

--- app/projects/remarkable/page.tsx
+++ app/projects/remarkable/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import NextProject from '../../components/NextProject';
 import MetaData from '../../components/MetaData';
-import quickfix from "@/public/img/remarkabkle/quickfix.gif"
+import ProjectHero from '@/app/components/ProjectHero';
 
 export const metadata = {
   title: "reMarkable Stacks | Carl Traberg",
@@ -11,12 +11,13 @@ export const metadata = {
 export default function Remarkable() {
   return <div className="content">
 
-    <p className="label">Time as paradigm</p>
-    <h1 className="heading-serif">reMarkable Stacks</h1>
-    <img
-      className='full-image'
-      src="/img/remarkable/0_main.JPG"
-      alt="reMarkable main image"
+    <ProjectHero
+      label="Time as paradigm"
+      title="reMarkable Stacks"
+      imageSrc="/img/remarkable/0_main.JPG"
+      imageAlt="reMarkable main image"
+      imageWidth={2104}
+      imageHeight={1600}
     />
 
     <MetaData
@@ -28,157 +29,179 @@ export default function Remarkable() {
 
     <div className='project-blocks'>
 
-      <div className='prose'>
-
+      <section className='case-section'>
         <p className='body-xl'><i>Stacks</i> is an exploratory concept for the UX of the reMarkable 2 device, exploring a more visual and transparent approach to navigating files.
-        </p></div>
+        </p></section>
 
-      <div className='prose'>
+      <section className='case-section'>
+        <div className="prose">
+          <p className="body-m">THE PROBLEM</p>
+          <p className='body-l'>
+            The existing UI had a traditional folder structure. This suits some, but it requires users to enter files and folders in order to see what is inside them. Users do not always remember where they wrote their last note.
+          </p></div>
 
-        <p className='body-l'>
-          The existing UI had a traditional folder structure. This suits some, but it requires users to enter files and folders in order to see what is inside them. Users do not always remember where they wrote their last note.
-        </p></div>
-
-              <Image
-        src="/img/remarkable/rigid.png"
-        className="small-image"
-        alt="rigid papers gif"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
-
-      <div className="prose">
-
-        <p className="body-l">
-          The task from reMarkable was to explore <i>time</i> as an organizational paradigm for the UI.
-        </p>
-      </div>
-
-      <div className="split">
-  <p className="body-l">
-    I interpreted time as chronology. Leading me to the idea of a stack of paper: older drawings at the bottom, newer on top.
-  </p>
-
-    <Image
-        src="/img/remarkable/stacking.gif"
-        className="split-image"
-        alt="Stacking papers gif"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
-</div>
-
-
-      {/* Core concept */}
-
-      {/* Supporting features */}
-
-      {/* Secondary features */}
+        <figure className="image-block image-block--small">
+          <Image
+            src="/img/remarkable/rigid.png"
+            className="full-image"
+            alt="The existing reMarkable folder interface"
+            width={1600}
+            height={1000}
+            sizes="(max-width: 900px) 100vw, 640px"
+          />
+          <figcaption className="caption">A rigid interface</figcaption>
+        </figure>
+      </section>
 
       {/* Quick fix*/}
-           <div className="prose">
+
+<section className='case-section'>
+      <div className="prose">
         <p className="body-m">QUICK FIX</p>
 
         <p className="body-l">
-          Many users complained about having to enter a file in order to view its content.
-        </p>
-
-        <p className="body-l ">
-          As a quick fix for reMarkable, I prototyped a swiping gesture to preview the content of files.
+          Many users complained about having to enter a file in order to view its content. As a quick fix for reMarkable, I prototyped a swiping gesture to preview the content of files.
         </p>
       </div>
-      <Image
-        src="/img/remarkable/quickfix.gif"
-        className="full-image"
-        alt="Swiping gesture to preview files"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
 
-      {/* Award*/}
-      <div className='prose'>
-        <p className='body-l'>
-          The project was nominated for the AHO WORKS AWARDS in the category Interaction Design.</p></div>
+       <figure className='image-block'>
+        <Image
+          src="/img/remarkable/quickfix.gif"
+          className="full-image"
+          alt="Swiping gesture to preview files"
+          width={1600}
+          height={1000}
+          sizes="(max-width: 900px) 100vw, 1000px"
+        />
+        <figcaption className='caption'>Swipe to preview</figcaption>
+        </figure>
+      </section>
 
-      <div className='prose'>
-        <p className='body-l'>
-          As an alternative to folders, Stacks has a tagging system making it possible to categorise a file into more than one category.
 
-          It also enable users to work in a “space” rather than a folder.</p></div>
+<section className='case-section'>
+      <div className="split">
 
-      <Image
-        src="/img/remarkable/image-02.png"
-        className="full-image"
-        alt="reMarkable Stacks tagging system"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
+        <div className='prose'>
+          <p className="body-m">EXPLORING TIME</p>
+          <p className="body-l">
+            The task from reMarkable was to explore <i>time</i> as an organisational paradigm for the UI.
+          </p>
+          <p className="body-l">
+            I interpreted time as chronology, which lead me to the idea of a stack of paper: older drawings at the bottom, newer on top.
+          </p>
+        </div>
 
-      <div className="prose">
+
+    <figure className="image-block image-block--small">
+        <Image
+          src="/img/remarkable/stacking.gif"
+          className="split-image"
+          alt="Stacking papers gif"
+          width={1600}
+          height={1000}
+          sizes="(max-width: 900px) 100vw, 1000px"
+        />
+          <figcaption className="caption">Old → New</figcaption>
+        </figure>
+      </div>
+      </section>
+
+      {/* Core concept */}
+
+    <section className='case-section'>
+      <div className="split">
+        <div className='prose'>
+          {/* <p className="body-m">PREVIEWING CONTENT</p> */}
         <p className="body-l">
           The re-design makes it possible to preview the content of a file before entering it, avoiding the problem of menu diving.
         </p>
+        </div>
+
+        <figure className='image-block'>
+        <Image
+          src="/img/remarkable/preview-content.gif"
+          className="full-image"
+          alt="Previewing file content in reMarkable Stacks"
+          width={1600}
+          height={1000}
+          sizes="(max-width: 900px) 100vw, 1000px"
+        />
+        <figcaption></figcaption>
+        </figure>
       </div>
+      </section>
 
-      <Image
-        src="/img/remarkable/image-03.png"
-        className="full-image"
-        alt="Previewing file content in reMarkable Stacks"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
+      {/* Supporting features */}
+<section className='case-section'>
+      <div className='split'>
 
+        <div className='prose'>
+          {/* <p className="body-m">STACKS</p> */}
+          <p className='body-l'>
+            As an alternative to folders, Stacks has a tagging system making it possible to categorise a file into more than one category.
 
+            It also enable users to work in a “space” rather than a folder.
+          </p>
+        </div>
 
-      <Image
-        src="/img/remarkable/image-04.png"
-        className="full-image"
-        alt="Existing reMarkable folder interface"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
+         <figure className="image-block image-block--small">
 
-      <Image
-        src="/img/remarkable/image-05.png"
-        className="full-image"
-        alt="Stack of paper interaction concept"
-        width={1600}
-        height={1000}
-        sizes="(max-width: 900px) 100vw, 1000px"
-      />
+        <Image
+          src="/img/remarkable/stacks.gif"
+          className="full-image"
+          alt="reMarkable Stacks tagging system"
+          width={1600}
+          height={1000}
+          sizes="(max-width: 900px) 100vw, 1000px"
+        />
 
-      <div className="prose">
-        <p className="body-m">Visible sidepanels</p>
+          <figcaption className="caption"></figcaption>
+        </figure>
+
       </div>
+</section>
 
+
+{/* SIDE PANELS HERE */}
+<section className="case-section">
+  <div className="split">
+    <figure className="image-block">
       <Image
-        src="/img/remarkable/image-06.png"
+        src="/img/remarkable/visible-sidepanels.png"
         className="full-image"
         alt="Visible sidepanels"
         width={1600}
         height={1000}
         sizes="(max-width: 900px) 100vw, 1000px"
+        style={{ border: "1px solid #e6e6e6" }}
       />
+      <figcaption className="caption">Visible sidepanels</figcaption>
+    </figure>
 
-      <div className="prose">
-        <p className="body-m">Hidden sidepanels</p>
-      </div>
-
+    <figure className="image-block">
       <Image
-        src="/img/remarkable/image-07.png"
+        src="/img/remarkable/hidden-sidepanels.jpg"
         className="full-image"
         alt="Hidden sidepanels"
         width={1600}
         height={1000}
         sizes="(max-width: 900px) 100vw, 1000px"
+        style={{ border: "1px solid #e6e6e6" }}
       />
+      <figcaption className="caption">Hidden sidepanels</figcaption>
+    </figure>
+  </div>
+</section>
 
+      {/* Secondary features */}
+
+      {/* Award*/}
+
+      <section className='case-section'>
+      <div className='prose'>
+        <p className='body-l'>
+          The project was nominated for the AHO WORKS AWARDS in the category Interaction Design.</p></div>
+          </section>
 
     </div>
 
